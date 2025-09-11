2ccb4693ad33a039bc3cbc123915b30508c14a49
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-09-11 15:11:52 +0700

    feat: add Praktikum 2

---
+++ week_03/src/praktikum2.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Langkah 1
+  int counter = 0;
+  while (counter < 5) {
+  print(counter);
+  counter++;
+  }
+
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 10);
+}
