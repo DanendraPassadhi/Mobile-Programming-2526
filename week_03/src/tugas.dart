eb0db6ca3b1d56fe07bc63feee83a086ad65999f
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-09-12 18:01:31 +0700

    feat: add tugas praktikum

--- week_03/src/tugas.dart
+++ week_03/src/tugas.dart
@@ -1,1 +1,27 @@
-// File tugas untuk week_03
+void main() {
+  String namaLengkap = "Danendra Nayaka Passadhi";
+  String nim = "2341720144";
+
+  print("Mencari bilangan prima antara 0 dan 201:");
+  print("----------------------------------------");
+
+  for (int i = 0; i <= 201; i++) {
+    if (isPrima(i)) {
+      print('$i $namaLengkap ($nim)');
+    } else {
+      print('$i');
+    }
+  }
+}
+
+bool isPrima(int n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
