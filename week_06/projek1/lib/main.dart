3b8015f7210832dff5a88a5cc42f78b5c9837190
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-10-02 18:16:23 +0700

    initial commit

---
+++ week_06/projek1/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Lorem Ipsum',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Ibu Kota Nusantara, Indonesia',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red),
+          const Text('7'),
+        ],
+      ),
+    );
+
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    return MaterialApp(
+      title: 'Flutter layout: Danendra Nayaka Passadhi dan 2341720144',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter layout demo')),
+        body: Column(children: [titleSection]),
+      ),
+    );
+  }
+}
