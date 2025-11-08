92d537c590c5ff3e13719bee9050bbda792086e0
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-11-08 14:24:47 +0700

    feat: Practical 8

--- week_10/books/lib/main.dart
+++ week_10/books/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
 import './geolocation.dart';
+import './navigation_first.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LocationScreen(),
+      home: const NavigationFirst(),
     );
   }
 }
