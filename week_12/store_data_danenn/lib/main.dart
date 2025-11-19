4bb6f002ddbcb0ea3b792403a9b211849c284df4
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-11-19 22:18:08 +0700

    W13: Jawaban Soal 3

--- week_12/store_data_danenn/lib/main.dart
+++ week_12/store_data_danenn/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'dart:convert';
+import './model/pizza.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -30,20 +33,30 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String pizzaString = '';
+  List<Pizza> myPizzas = [];
 
   @override
   void initState() {
     super.initState();
-    readJsonFile();
+    readJsonFile().then((value) {
+      setState(() {
+        myPizzas = value;
+      });
+    });
   }
 
-  Future readJsonFile() async {
+  Future<List<Pizza>> readJsonFile() async {
     String myString = await DefaultAssetBundle.of(
       context,
     ).loadString('assets/pizzalist.json');
-    setState(() {
-      pizzaString = myString;
-    });
+    List pizzaMapList = jsonDecode(myString);
+
+    List<Pizza> myPizzas = [];
+    for (var pizza in pizzaMapList) {
+      Pizza myPizza = Pizza.fromJson(pizza);
+      myPizzas.add(myPizza);
+    }
+    return myPizzas;
   }
 
   @override
@@ -54,12 +67,15 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).primaryColor,
         foregroundColor: Colors.white,
       ),
-      body: pizzaString.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(pizzaString),
-            ),
+      body: ListView.builder(
+        itemCount: myPizzas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(myPizzas[index].pizzaName),
+            subtitle: Text(myPizzas[index].description),
+          );
+        },
+      ),
     );
   }
 }
